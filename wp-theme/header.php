b412d6304a26bafe49d16fce96e08c37274b6b27
vavlxxx <vladimir.nefedov357@gmail.com>
2026-04-26 20:54:10 +0500

    добавил скрипт яндекс метрики прямо в header.php

--- wp-theme/header.php
+++ wp-theme/header.php
@@ -9,8 +9,21 @@
   <title><?php echo get_the_title();?></title>
   <?php wp_head(); ?>
   <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+  
   <!-- Yandex.Metrika counter -->
-  <!-- /Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108008534', 'ym');
+
+    ym(108008534, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/108008534" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </head>
 <body <?php body_class('site-loading'); ?>>
   <?php wp_body_open(); ?>
